bcc079f4cd99f5bcc7325c5612367f57bd10fe93
lasha Tarkhnihvili <lasha.tarkhnishvili@yahoo.com>
2021-03-10 12:51:06 +0400

    shashi

--- script.js
+++ script.js
@@ -20,7 +20,6 @@ $(document).ready(function(){
     // toggle menu/navbar script
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
     });
 
     // typing animation script
